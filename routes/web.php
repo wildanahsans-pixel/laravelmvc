e6a532ab5e88a045551a35845796c48449c06849
Wildan <wildansairo@gmail.com>
2026-05-12 14:46:58 +0700

    add mvc implementation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
